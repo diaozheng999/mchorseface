d70d040dd0f53982a3ba94776b35161bbf2398cb
Jonathan Merrin <Jonmerrin@gmail.com>
2016-09-16 01:27:24 -0400

    darts now point down when falling and stick on landing

---
+++ LawnDart/Assets/Scripts/DartController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DartController : MonoBehaviour {
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.LookAt(transform.position + rb.velocity);
+        //gameObject.transform.eulerAngles = rb.velocity;
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
